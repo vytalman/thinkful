1caf7af2d02fb71f37028d7f2532f26267ddaf32
vytalman <ryanmesa@gmail.com>
2014-09-24 14:06:29 -0400

    Initial Commit

---
+++ Unit2Lesson3/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Unit2Lesson3
+//
+//  Created by Ryan Mesa on 9/24/14.
+//  Copyright (c) 2014 Ryan Mesa. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
