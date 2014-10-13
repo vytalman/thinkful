6962e32295a3227334093721389cd6fa4b94aec5
vytalman <ryanmesa@gmail.com>
2014-10-12 22:04:24 -0400

    Assignment 4

---
+++ Unit3Lesson1/Unit3Lesson1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Unit3Lesson1
+//
+//  Created by Ryan Mesa on 10/2/14.
+//  Copyright (c) 2014 Ryan Mesa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
